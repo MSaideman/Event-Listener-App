e4c2e8a389c369855449ca2dd1dc1ad243c8e4e9
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-09-18 11:02:11 -0600

    added delete js file

--- public/js/create.js
+++ public/js/create.js
@@ -1,23 +1,27 @@
-// function to create post
-
-async function postForm(event) {
+const newFormHandler = async (event) => {
     event.preventDefault();
-    const title = document.querySelector("#title").value
-    const content = document.querySelector("#content").value
-
-        const response = await fetch('/api/pevents/', {
-            method:"post",
-            headers:{'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                title: title,
-                content: content
-            })
-        })
-        if (response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert(response.statusText);
-        }
+  
+    const name = document.querySelector('#title').value.trim();
+    // const needed_funding = document.querySelector('#project-funding').value.trim();
+    const description = document.querySelector('#content').value.trim();
+  
+    if (name && description) {
+      const response = await fetch(`/api/pevents`, {
+        method: 'POST',
+        body: JSON.stringify({ name,description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create event');
+      }
     }
-
-document.querySelector('.postBody').addEventListener('submit', postForm);
+  };
+  
+  document
+    .querySelector('.new-pevent-form')
+    .addEventListener('submit', newFormHandler);
