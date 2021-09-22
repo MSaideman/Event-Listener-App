032e3c176775f27b33bafbb7d9d494e61adb2901
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-09-21 22:12:17 -0600

    added port info on server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
@@ -35,6 +36,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+app.set("port", PORT);
 // this false its for to do no change and true do any change on database as key
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
